95961d2a1eaecae2746bb64fab61c9c0accd6ebb
Arthur Francisco Davantel da silva <arthurdavantel2@gmail.com>
2025-08-07 22:00:44 -0300

    função login

--- MilkApi/Controllers/UsuarioController.cs
+++ MilkApi/Controllers/UsuarioController.cs
@@ -7,7 +7,7 @@ namespace MilkApi.Controllers
     [Route("[controller]")]
     public class UsuarioController : Controller
     {
-        private const string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BancoTccGado;Integrated Security=True;";
+        private const string ConnectionString = "Server=milkdatabase.cp64yi8w2sr2.us-east-2.rds.amazonaws.com;Database=BancoTccGado;User Id=Arthur;Password=Arthur-1234;TrustServerCertificate=True;";
         private readonly ILogger<UsuarioController> _logger;
 
         public UsuarioController(ILogger<UsuarioController> logger)
