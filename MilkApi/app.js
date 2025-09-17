c61b2f6ea8c3d919c0578a132f0fef8595b65f11
ThurzinDeix <arthurdavantel2@gmail.com>
2025-09-17 11:39:05 -0300

    nav bar responsiva e numero tentativas de reprodução

--- MilkApi/app.js
+++ MilkApi/app.js
@@ -1,15 +1,16 @@
-﻿function checkAuthentication() {
-    // Verifique se o usuário está autenticado comparando com o valor no localStorage
-    const isAuthenticated = localStorage.getItem('authenticated');
-
-    // Se isAuthenticated for verdadeiro, o usuário está autenticado
-    return isAuthenticated === 'true';
-}
-
-// Verificar a autenticação antes de permitir o acesso à dashboard
-window.addEventListener('DOMContentLoaded', () => {
-    if (!checkAuthentication()) {
-        // Se o usuário não estiver autenticado, redirecione para a página de login
-        window.location.href = 'login.html';
+﻿// auth.js
+function logout() {
+    localStorage.removeItem('authenticated');
+    localStorage.removeItem('userId');
+    alert("Você saiu da conta.");
+    window.location.href = "login.html";
+  }
+  
+  // Protege páginas internas
+  window.onload = function() {
+    const autenticado = localStorage.getItem('authenticated');
+    if (!autenticado && !window.location.href.includes("login.html")) {
+      window.location.href = "login.html";
     }
-});
+  }
+  
--- MilkApi/config.cs
+++ MilkApi/config.cs
@@ -1,9 +1,12 @@
-﻿namespace MilkApi
+﻿using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace MilkApi
 {
     public class config
     {   
         public static string ConnectionString { get; } =
         "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BancoTccGado;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-
     }
 }
